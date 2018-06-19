576aa7e25f9cd9b4be32e6ee20aec0a8bbe6fef1
manojkumarsmks <manojkumarsmks@gmail.com>
2018-06-19 15:42:07 -0600

    Few changes  with return type

--- StringRabinKarp.java
+++ StringRabinKarp.java
@@ -2,24 +2,50 @@
 public class StringRabinKarp {
 	
 	private int primeNumber = 3;
+	int oldHash = 0, patternHash = 0;
+	String string, pattern;
 	
+	// Constructor
+	public StringRabinKarp(String string, String pattern) {
+		this.string = string;
+		this.pattern = pattern;
+	}
+	
+	
+	// Basics algorithm 
+	public int patternMatching() {
+		oldHash = Hashing(string, 3);
+		patternHash =  Hashing(pattern, pattern.length());
+		
+		if(oldHash == patternHash) {
+			System.out.println(checkEquals(string, pattern, 0));
+		}
+		
+		for (int i = 1; i <= string.length()-3; i++) {
+			oldHash = recalcuateHashing(string, oldHash, i, 3);
+		
+			if(oldHash == patternHash) {
+				if(checkEquals(string, pattern, i))
+					return i;
+			}
+		}
+		
+		return -1;
+	}
+
 	// Calculate the first Hashing
 	public int Hashing(String S, int m) {
 		int result = 0;
 		S = S.substring(0, m);
-		for (int i = 0; i < S.length(); i++) {
-			
+		for (int i = 0; i < S.length(); i++) {	
 			result += (S.charAt(i)*Math.pow(primeNumber, i));
-
 		}
-		
 		return result;
 	}
 	
 	// Recalculate the hashing
 	public int recalcuateHashing(String S, int oldHash, int start, int m) {
 		
-		String subString = S.substring(start, start+m);
 		oldHash = (oldHash - S.charAt(start-1))/primeNumber;
 		oldHash += S.charAt(start+m-1) * Math.pow(primeNumber, m-1);
 		
@@ -43,30 +69,14 @@ public class StringRabinKarp {
 	
 	public static void main(String[] args) {
 		
-		int oldHash = 0, patternHash = 0;
-		String string = "ABCDEF";
-		String pattern = "CDE";
 		
-		StringRabinKarp stringRabinKarp = new StringRabinKarp();
+		String string = "ManojKumar ";
+		String pattern = "Kum";
 		
-		oldHash = stringRabinKarp.Hashing(string, 3);
-		patternHash =  stringRabinKarp.Hashing(pattern, pattern.length());
+		StringRabinKarp stringRabinKarp = new StringRabinKarp(string, pattern);
 		
-		if(oldHash == patternHash) {
-			System.out.println(stringRabinKarp.checkEquals(string, pattern, 0));
-		}
+		System.out.println(stringRabinKarp.patternMatching());
 		
-		for (int i = 1; i <= string.length()-3; i++) {
-			oldHash = stringRabinKarp.recalcuateHashing(string, oldHash, i, 3);
 		
-			if(oldHash == patternHash) {
-				if(stringRabinKarp.checkEquals(string, pattern, i))
-					System.out.println("Pattern starts at "+i);
-				else 
-					System.out.println("Pattern not found in the String");
-					
-					
-			}
-		}
 	}
 }
